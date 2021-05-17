8c66e1b0b05201596f3c492531c9f0be62a6d05a
Scarecrow0w <daniil6547@ya.ru>
2021-05-17 16:52:13 +0300

    adaptive fixes, navbar works

--- main.js
+++ main.js
@@ -1,49 +1,26 @@
-// let groupCards = document.querySelectorAll('.card-group-section'),
-// storiesCards = document.querySelectorAll('.card-stories-section'),
-// newsCards = document.querySelectorAll('.card-news-section');
-
-// const cards = [groupCards, storiesCards, newsCards]
-
-
-// for (j = 0; j < cards.length; j++) {
-
-//   cards[j].forEach((elem)=>{
-//     elem.addEventListener('mouseover', () => {
-//       elem.classList.add('active')
-      
-//       cards[0].forEach((item) => {
-//         if (!item.classList.contains('active')) item.classList.add('inactive')
-//       })
-  
-//       elem.addEventListener('mouseout', () => {
-//         cards[0].forEach((item) => {
-//           item.classList.remove('active')
-//           item.classList.remove('inactive')
-//         })
-//       }) 
-//     })  
-//   })
-// }
-
+// Добавление классов эффектов при наведении на карточки
 const cards = document.querySelectorAll('.card')
 
 cards.forEach((elem)=>{
   elem.addEventListener('mouseover', () => {
-    elem.classList.add('active')
+    const parent = elem.parentElement,
+    thisCards = parent.querySelectorAll('.card');
     
-    cards.forEach((item) => {
-      if (!item.classList.contains('active')) item.classList.add('inactive')
+    thisCards.forEach((el) => {
+      el.classList.add('inactive')
     })
 
-    elem.addEventListener('mouseout', () => {
-      cards.forEach((item) => {
-        item.classList.remove('active')
-        item.classList.remove('inactive')
-      })
-    }) 
+    elem.classList.remove('inactive')
+    elem.classList.add('active')
   })  
-})
 
+  elem.addEventListener('mouseout', () => {
+    cards.forEach((item) => {
+      item.classList.remove('active')
+      item.classList.remove('inactive')
+    })
+  }) 
+})
 
 
 
@@ -67,3 +44,569 @@ window.addEventListener('scroll', () => {
 
   navbar.classList.toggle('fixed', window.scrollY > 0)
 })
+
+// функция Hamburger Menu
+function navToggle() {
+  let btn = document.getElementById('menuBtn'),
+  nav = document.getElementById('menu');
+
+  btn.classList.toggle('open');
+  nav.classList.toggle('flex');
+  nav.classList.toggle('hidden');
+}
+
+
+const links = {
+  
+    Group: {
+      name: 'Group',
+      url: '#',
+      title: {
+          name: 'Who we are',
+          url: '#'
+      },
+      lists: [
+        {
+          list_1: {       
+              title: 'About Emi',
+              url: '#',     
+              items: [
+              {
+                  item_1: {
+                  title: 'Our purpose',
+                  url: '#'
+                  }
+              },
+              {  
+                  item_2: {
+                  title: 'Strategy & Model',
+                  url: '#'
+                  }
+              },
+              {
+                  item_3: {
+                  title: 'Quality & Safety Standards',
+                  url: '#'
+                  }
+              },
+              { 
+                  item_4: {
+                  title: 'Our performance',
+                  url: '#'
+                  }
+              }              
+              ]
+          }
+        },
+        {
+          list_2: {       
+              title: 'Governance & Ethics',
+              url: '#',     
+              items: [
+              {
+                  item_1: {
+                  title: 'Board of directors',
+                  url: '#'
+                  }
+              },
+              {  
+                  item_2: {
+                  title: 'Executive Committee',
+                  url: '#'
+                  }
+              },
+              {
+                  item_3: {
+                  title: 'Our ethical principles',
+                  url: '#'
+                  }
+              }             
+              ]
+          }
+        },
+        {
+          list_3: {       
+              title: 'Culture & Heritage',
+              url: '#',     
+              items: [
+              {
+                  item_1: {
+                  title: 'Life at Emi',
+                  url: '#'
+                  }
+              },
+              {  
+                  item_2: {
+                  title: 'Values & Mindset',
+                  url: '#'
+                  }
+              },
+              {
+                  item_3: {
+                  title: 'Emi History',
+                  url: '#'
+                  }
+              }             
+              ]
+          }
+        },
+        {
+          list_4: {       
+            title: 'News & Documentation',
+            url: '#',     
+            items: [
+              {
+                item_1: {
+                title: 'News',
+                url: '#'
+                }
+              },
+              {  
+                item_2: {
+                title: 'Publications',
+                url: '#'
+                }
+              },
+              {
+                item_3: {
+                title: 'Media Library',
+                url: '#'
+                }
+              },
+              { 
+                item_4: {
+                title: 'Press Releases',
+                url: '#'
+                }
+              }              
+            ]
+          }
+        }
+      ]
+    },
+  
+    Commitments: {
+      name: 'Commitments',
+      url: '#',
+      title: {
+          name: 'Our Commitments & Responsibilities',
+          url: '#'
+      },
+      lists: [
+        {
+          list_1: {       
+            title: 'For the Planet',
+            url: '#',     
+            items: [
+              {
+                item_1: {
+                title: 'Our purpose',
+                url: '#'
+                }
+              },
+              {  
+                item_2: {
+                title: 'Strategy & Model',
+                url: '#'
+                }
+              },
+              {
+                item_3: {
+                title: 'Quality & Safety Standards',
+                url: '#'
+                }
+              },
+              { 
+                item_4: {
+                title: 'Our performance',
+                url: '#'
+                }
+              }              
+            ]
+          }
+        },
+        {
+          list_2: {       
+              title: 'Governance & Ethics',
+              url: '#',     
+              items: [
+              {
+                  item_1: {
+                  title: 'Board of directors',
+                  url: '#'
+                  }
+              },
+              {  
+                  item_2: {
+                  title: 'Executive Committee',
+                  url: '#'
+                  }
+              },
+              {
+                  item_3: {
+                  title: 'Our ethical principles',
+                  url: '#'
+                  }
+              }             
+              ]
+          }
+        },
+        {
+          list_3: {       
+              title: 'Culture & Heritage',
+              url: '#',     
+              items: [
+              {
+                  item_1: {
+                  title: 'Life at Emi',
+                  url: '#'
+                  }
+              },
+              {  
+                  item_2: {
+                  title: 'Values & Mindset',
+                  url: '#'
+                  }
+              },
+              {
+                  item_3: {
+                  title: 'Emi History',
+                  url: '#'
+                  }
+              }             
+              ]
+          }
+        },
+        {
+          list_4: {       
+              title: 'News & Documentation',
+              url: '#',     
+              items: [
+              {
+                  item_1: {
+                  title: 'News',
+                  url: '#'
+                  }
+              },
+              {  
+                  item_2: {
+                  title: 'Publications',
+                  url: '#'
+                  }
+              },
+              {
+                  item_3: {
+                  title: 'Media Library',
+                  url: '#'
+                  }
+              },
+              { 
+                  item_4: {
+                  title: 'Press Releases',
+                  url: '#'
+                  }
+              }              
+              ]
+          }
+        }
+      ]
+    },
+  
+    Brands: {
+      name: 'Brands',
+      url: '#',
+      title: {
+          name: 'Our Global Brands Portfolio',
+          url: '#'
+      },
+      lists: [
+        {
+          list_1: {       
+              title: 'Consumer Products',
+              url: '#',     
+              items: [
+              {
+                  item_1: {
+                  title: 'Our purpose',
+                  url: '#'
+                  }
+              },
+              {  
+                  item_2: {
+                  title: 'Strategy & Model',
+                  url: '#'
+                  }
+              },
+              {
+                  item_3: {
+                  title: 'Quality & Safety Standards',
+                  url: '#'
+                  }
+              },
+              { 
+                  item_4: {
+                  title: 'Our performance',
+                  url: '#'
+                  }
+              }              
+              ]
+          }
+        },
+        {
+          list_2: {       
+              title: 'Emi Luxe',
+              url: '#', 
+              itemsTitle: {
+              title: 'Explore Emi Luxe Division',
+              url: '#'
+              },     
+              items: [
+              {
+                  item_1: {
+                  title: 'Board of directors',
+                  url: '#'
+                  }
+              },
+              {  
+                  item_2: {
+                  title: 'Executive Committee',
+                  url: '#'
+                  }
+              },
+              {
+                  item_3: {
+                  title: 'Our ethical principles',
+                  url: '#'
+                  }
+              }             
+              ]
+          }
+        },
+        {
+          list_3: {       
+            title: 'Profession Products',
+            url: '#',   
+            itemsTitle: {
+              title: 'Explore Professional Products Division',
+              url: '#'
+            },  
+            items: [
+              {
+                item_1: {
+                title: 'Life at Emi',
+                url: '#'
+                }
+              },
+              {  
+                item_2: {
+                title: 'Values & Mindset',
+                url: '#'
+                }
+              },
+              {
+                item_3: {
+                title: 'Emi History',
+                url: '#'
+                }
+              }             
+            ]
+          }
+        },
+        {
+          list_4: {       
+            title: 'Active Cosmetics',
+            url: '#',   
+            itemsTitle: {
+              title: 'Explore Active Cosmetics Division',
+              url: '#'
+            },   
+            items: [
+              {
+                item_1: {
+                title: 'News',
+                url: '#'
+                }
+              },
+              {  
+                item_2: {
+                title: 'Publications',
+                url: '#'
+                }
+              },
+              {
+                item_3: {
+                title: 'Media Library',
+                url: '#'
+                }
+              },
+              { 
+                item_4: {
+                title: 'Press Releases',
+                url: '#'
+                }
+              }              
+            ]
+          }
+        }
+      ]
+    },
+  
+    Beauty: {
+    name: 'Beauty Science & Technology',
+    url: '#',
+    title: {
+        name: 'How to Invent Future of Beauty',
+        url: '#'
+    },
+    lists: [
+        {
+        list_1: {       
+            title: 'Beauty Research & Innovation',
+            url: '#',     
+            items: [
+            {
+                item_1: {
+                title: 'Our purpose',
+                url: '#'
+                }
+            },
+            {  
+                item_2: {
+                title: 'Strategy & Model',
+                url: '#'
+                }
+            },
+            {
+                item_3: {
+                title: 'Quality & Safety Standards',
+                url: '#'
+                }
+            },
+            { 
+                item_4: {
+                title: 'Our performance',
+                url: '#'
+                }
+            }              
+            ]
+        }
+        },
+        {
+        list_2: {       
+            title: 'Governance & Ethics',
+            url: '#',     
+            items: [
+            {
+                item_1: {
+                title: 'Board of directors',
+                url: '#'
+                }
+            },
+            {  
+                item_2: {
+                title: 'Executive Committee',
+                url: '#'
+                }
+            },
+            {
+                item_3: {
+                title: 'Our ethical principles',
+                url: '#'
+                }
+            }             
+            ]
+        }
+        },
+        {
+        list_3: {       
+            title: 'Culture & Heritage',
+            url: '#',     
+            items: [
+            {
+                item_1: {
+                title: 'Life at Emi',
+                url: '#'
+                }
+            },
+            {  
+                item_2: {
+                title: 'Values & Mindset',
+                url: '#'
+                }
+            },
+            {
+                item_3: {
+                title: 'Emi History',
+                url: '#'
+                }
+            }             
+            ]
+        }
+        },
+        {
+        list_4: {       
+            title: 'News & Documentation',
+            url: '#',     
+            items: [
+            {
+                item_1: {
+                title: 'News',
+                url: '#'
+                }
+            },
+            {  
+                item_2: {
+                title: 'Publications',
+                url: '#'
+                }
+            },
+            {
+                item_3: {
+                title: 'Media Library',
+                url: '#'
+                }
+            },
+            { 
+                item_4: {
+                title: 'Press Releases',
+                url: '#'
+                }
+            }              
+            ]
+        }
+        }
+      ]
+    },
+  
+    Careers: {
+    title: 'Careers',
+    url: '#'
+    }
+  
+}
+
+console.log(links);
+
+// let linkTitle = document.querySelector('.link-title')
+
+// linkTitle.textContent = '123'
+
+let navlink = document.querySelectorAll('.nav-link'),
+submenu = document.querySelector('.submenu');
+
+// Показать подменю при наведении курсора на ссылку
+navlink.forEach((el) => {
+  
+
+  el.addEventListener('mouseover', () => {
+    submenu.classList.remove('hidden')
+    submenu.classList.add('flex')
+  })
+})
+
+// Скрыть подменю при mouseleave submenu
+submenu.addEventListener('mouseleave', () => {
+  submenu.classList.remove('flex')
+  submenu.classList.add('hidden')
+})
+
